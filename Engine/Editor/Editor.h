b924d2a355d1a975e2b33b70d11073a9f9398372
huishan.s <huishan0719@gmail.com>
2025-10-21 11:28:41 +0800

    Update Editor
    Add Files:
    - Editor.cpp/h
    ----------------------------------
    1. Add basic function
    To Be Do:
    - Figure out how to set the start up for editor

---
+++ Engine/Editor/Editor.h
@@ -0,0 +1,63 @@
+#pragma once
+/**
+ * @file ImguiManager.h
+ * @brief Declaration of the IMGUI_Manager class for running the IMGUI level editor.
+ * @author Liliana Hanawardani (45%), Saw Hui Shan (45%), Rio Shannon Yvon Leonardo (10%)
+ * @date September 8, 2025
+ * Copyright (C) 2025 DigiPen Institute of Technology.
+ * Reproduction or disclosure of this file or its contents without the
+ * prior written consent of DigiPen Institute of Technology is prohibited.
+ */
+
+#ifndef SK_IMGUI_MANAGER_H
+#define SK_IMGUI_MANAGER_H
+
+// Include Editor Header Files
+#include "../IMGUI/imgui.h"
+#include "../IMGUI/imgui_impl_glfw.h"
+#include "../IMGUI/imgui_impl_opengl3.h"
+
+// Include Standard Headers
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <algorithm>
+
+
+// Include other necessary headers
+
+namespace Engine
+{
+	/**
+	* @class Editor
+	* @brief Displaying and editing information, and the internal logic in level editor.
+	*/
+	class Editor
+	{
+	private:
+
+		
+
+	public:
+		// Default contructor 
+		Editor() {};
+
+		// Deconstuctor
+		~Editor() = default;
+
+		// Delected copy constructor
+		Editor(const Editor&) = delete;
+
+		// Deleted copy assignment operator
+		Editor& operator=(const Editor&) = delete;
+
+		// still figure out
+		//int starup();
+	};
+
+
+} // end of gam300
+
+
+#endif // LOF_IMGUI_MANAGER_h
