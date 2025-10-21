3c82892ca657137bb72cc4843614124266811224
huishan.s <huishan0719@gmail.com>
2025-10-21 15:21:32 +0800

    Update
    Uncomment the ImGuiPart

--- Engine/Editor/Editor.cpp
+++ Engine/Editor/Editor.cpp
@@ -30,8 +30,8 @@ namespace Engine
 		// Set config flag
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-		//io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
@@ -40,11 +40,11 @@ namespace Engine
 		ImGuiStyle& style = ImGui::GetStyle();
 
 		// Set WindowRounding and ImGuiCol_WindowBg when viewport is enabled
-		/*if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			style.WindowRounding = 0.0f;
 			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-		}*/
+		}
 
 		// Setup Platform/Renderer backends
 		ImGui_ImplGlfw_InitForOpenGL(m_Window, true);
