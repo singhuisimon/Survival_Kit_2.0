0ff2ff089b0f4fa8c9dfc025080acacf12ee34e2
huishan.s <huishan0719@gmail.com>
2025-11-08 20:10:12 +0800

    Update Editor.cpp
    Update Light Component in Property Panel

--- Engine/Editor/Editor.cpp
+++ Engine/Editor/Editor.cpp
@@ -14,6 +14,7 @@
 #include "../Component/TransformComponent.h"
 #include "../Component/ParticleComponent.h"
 #include "../Transform/TransformSystem.h"
+#include "../Component/LightComponent.h"
 
 #include "../Serialization/SceneSerializer.h"
 #include "../Serialization/PrefabSerializer.h"
@@ -837,6 +838,8 @@ namespace Engine
 						auto& audio = m_SelectedEntity.GetComponent<AudioComponent>();
 
 						ImGui::Separator();
+					/*	ImGui::Columns(2, nullptr, false);
+						ImGui::SetColumnWidth(0, 200.0f);*/
 
 						auto& db = AM.db();
 						auto allAssets = db.AllMutable();
@@ -981,10 +984,14 @@ namespace Engine
 					{
 						m_SelectedEntity.RemoveComponent<AudioComponent>();
 					}
+					
 				}
+				// ========================== Display ReverbZoneComponent =====================================
 				if (m_SelectedEntity.HasComponent<ReverbZoneComponent>())
 				{
 					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
 
 					bool openReverbComponent = ImGui::CollapsingHeader("Reverb Zone Component", ImGuiTreeNodeFlags_DefaultOpen);
 					bool removeReverb = false;
@@ -1008,11 +1015,8 @@ namespace Engine
 
 					ImGui::Columns(1);
 
-					if (removeReverb)
+					if (openReverbComponent) 
 					{
-						m_SelectedEntity.RemoveComponent<ReverbZoneComponent>();
-					
-					} else if (openReverbComponent) {
 						auto& reverbZone = m_SelectedEntity.GetComponent<ReverbZoneComponent>();
 						
 						const char* presets[] = { "Custom", "Generic", "Bathroom", "Room", "Cave", "Arena" };	
@@ -1076,10 +1080,19 @@ namespace Engine
 							reverbZone.SetMaxDistance(maxDistanceReverb);
 						}	
 					}
+					//---------------------- Remove ReverbZone Component by ... -------------------------
+					if (removeReverb)
+					{
+						m_SelectedEntity.RemoveComponent<ReverbZoneComponent>();
+
+					}
 				}
+				// ====================================== Display ListenerComponent ==================================
 				if (m_SelectedEntity.HasComponent<ListenerComponent>())
 				{
 					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
 
 					bool openListenerComponent = ImGui::CollapsingHeader("Listener Component", ImGuiTreeNodeFlags_DefaultOpen);
 					bool removeListener = false;
@@ -1102,11 +1115,8 @@ namespace Engine
 
 					ImGui::Columns(1);
 
-					if (removeListener)
+					if (openListenerComponent)
 					{
-						m_SelectedEntity.RemoveComponent<ListenerComponent>();
-					
-					} else if (openListenerComponent) {
 						auto& listener = m_SelectedEntity.GetComponent<ListenerComponent>();
 						bool& active = listener.Active;
 
@@ -1114,14 +1124,43 @@ namespace Engine
 							listener.Active = active;
 						}
 					}
+					// -------------------------- Remove ListernerComponent -------------------------
+					if (removeListener)
+					{
+						m_SelectedEntity.RemoveComponent<ListenerComponent>();
+
+					}
 				}
+				// =============================== Display BT Component =========================
 				if (m_SelectedEntity.HasComponent<BehaviourTreeComponent>())
 				{
 					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
 
 					bool openBTComponent = ImGui::CollapsingHeader("Behaviour Tree Component", ImGuiTreeNodeFlags_DefaultOpen);
+					bool removeBTComponent = false;
 
-					if (openBTComponent) {
+					ImGui::NextColumn();
+
+					if (ImGui::Button("...###BTBtn", dotButtonSize))
+					{
+						ImGui::OpenPopup("BTPopUp");
+					}
+					if (ImGui::BeginPopup("BTPopUp"))
+					{
+						if (ImGui::MenuItem("Remove Component"))
+						{
+							removeBTComponent = true;
+							//return;
+						}
+						ImGui::EndPopup();
+					}
+
+					ImGui::Columns(1);
+
+					if (openBTComponent) 
+					{
 
 						auto& ai_bt = m_SelectedEntity.GetComponent<BehaviourTreeComponent>();
 
@@ -1314,27 +1353,8 @@ namespace Engine
 						CloneTree
 						*/
 					}
-
-					bool removeBT = false;
-
-					ImGui::NextColumn();
-
-					if (ImGui::Button("... ###BehaviorbBtn", dotButtonSize))
-					{
-						ImGui::OpenPopup("BehaviorPopUp");
-					}
-					if (ImGui::BeginPopup("BehaviorPopUp"))
-					{
-						if (ImGui::MenuItem("Remove Component"))
-						{
-							removeBT = true;
-						}
-						ImGui::EndPopup();
-					}
-
-					ImGui::Columns(1);
-
-					if (removeBT)
+					// ----------------------------------- Remove BT Component -----------------------
+					if (removeBTComponent)
 					{
 						m_SelectedEntity.RemoveComponent<BehaviourTreeComponent>();
 					}
@@ -1344,9 +1364,34 @@ namespace Engine
 				if (m_SelectedEntity.HasComponent<ParticleComponent>())
 				{
 					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
+
+					bool openParticleComp = ImGui::CollapsingHeader("Particle System", ImGuiTreeNodeFlags_DefaultOpen);
+					bool removeParticleComp = false;
+
 					auto& particleComp = m_SelectedEntity.GetComponent<ParticleComponent>();
 
-					if (ImGui::CollapsingHeader("Particle System", ImGuiTreeNodeFlags_DefaultOpen))
+					ImGui::NextColumn();
+					//ImGui::SameLine(ImGui::GetWindowContentRegionMax().x - 30);
+
+					if (ImGui::Button("...###ParticleBtn", dotButtonSize))
+					{
+						ImGui::OpenPopup("ParticlePopUp");
+					}
+					if (ImGui::BeginPopup("ParticlePopUp"))
+					{
+						if (ImGui::MenuItem("Remove Component"))
+						{
+							removeParticleComp = true;
+							//return;
+						}
+						ImGui::EndPopup();
+					}
+
+					ImGui::Columns(1);
+
+					if (openParticleComp)
 					{
 						// Playback Controls
 						ImGui::Text("Playback");
@@ -1466,16 +1511,46 @@ namespace Engine
 							particleComp.EmissionAccumulator = 0.0f;
 						}
 					}
+					// ----------------------------------------- Remove Particle Component -------------------------------
+					if (removeParticleComp)
+					{
+						m_SelectedEntity.RemoveComponent<ParticleComponent>();
+					}
 				}
 				// ========================= Display Script Compoment ===============================
 				if (m_SelectedEntity.HasComponent<ScriptComponent>())
 				{
 					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
+					
+					bool openScriptComp = ImGui::CollapsingHeader("Script Component", ImGuiTreeNodeFlags_DefaultOpen);
+					bool removeScriptComp = false;
+
+
 					auto& scriptComp = m_SelectedEntity.GetComponent<ScriptComponent>();
 					std::string scriptPath = getRepository() + "\\Scripts\\Game";
 					auto scriptFiles = getAssetsInFolder(scriptPath);
 					
-					if (ImGui::CollapsingHeader("Script Component", ImGuiTreeNodeFlags_DefaultOpen))
+					ImGui::NextColumn();
+
+					if (ImGui::Button("...###ScriptBtn", dotButtonSize))
+					{
+						ImGui::OpenPopup("ScriptPopUp");
+					}
+					if (ImGui::BeginPopup("ScriptPopUp"))
+					{
+						if (ImGui::MenuItem("Remove Component"))
+						{
+							removeScriptComp = true;
+							//return;
+						}
+						ImGui::EndPopup();
+					}
+
+					ImGui::Columns(1);
+
+					if (openScriptComp)
 					{
 						ImGui::Text("Instance: %s", scriptComp.ScriptInstance ? "Active" : "None");
 						ImGui::Text("Started: %s", scriptComp.Started ? "Yes" : "No");
@@ -1531,9 +1606,104 @@ namespace Engine
 						}
 
 					}
+					// ----------------------------- Remove Script Comp --------------------------------------
+					if (removeScriptComp)
+					{
+						m_SelectedEntity.RemoveComponent<ScriptComponent>();
+					}
 
 				}
+				// ================================ Display Light Component ======================================
+				if (m_SelectedEntity.HasComponent<LightComponent>())
+				{
+					ImGui::Separator();
+					ImGui::Columns(2, nullptr, false);
+					ImGui::SetColumnWidth(0, 200.0f);
 
+					bool openLightComp = ImGui::CollapsingHeader("Light Component", ImGuiTreeNodeFlags_DefaultOpen);
+					bool removeLightComp = false;
+
+					ImGui::NextColumn();
+					//ImGui::SameLine(ImGui::GetWindowContentRegionMax().x - 30);
+
+					if (ImGui::Button("...###LightBtn", dotButtonSize))
+					{
+						ImGui::OpenPopup("LightPopUp");
+					}
+					if (ImGui::BeginPopup("LightPopUp"))
+					{
+						if (ImGui::MenuItem("Remove Component"))
+						{
+							removeLightComp = true;
+							//return;
+						}
+						ImGui::EndPopup();
+					}
+
+					ImGui::Columns(1);
+
+					if (openLightComp)
+					{
+						auto& lightComp = m_SelectedEntity.GetComponent<LightComponent>();
+						ImGui::Checkbox("Enabled", &lightComp.Enabled);
+
+						// --- Light Type Dropdown ---
+						const char* lightTypeNames[] = { "Directional", "Point", "Spot" };
+						int currentType = static_cast<int>(lightComp.Type);
+
+						if (ImGui::Combo("Type", &currentType, lightTypeNames, IM_ARRAYSIZE(lightTypeNames)))
+						{
+							lightComp.SetType(static_cast<LightType>(currentType));
+						}
+
+						// --- Color ---
+						glm::vec3 color = lightComp.Color;
+						if (ImGui::ColorEdit3("Color", glm::value_ptr(color)))
+						{
+							lightComp.SetColorLinear(color); // uses setter
+						}
+
+						
+						// --- Intensity ---
+						float intensity = lightComp.Intensity;
+						if (ImGui::DragFloat("Intensity", &intensity, 0.05f, 0.0f, 100.0f, "%.2f"))
+						{
+							lightComp.SetIntensity(intensity); //  uses setter
+						}
+
+
+						// --- Range  ---
+						if (lightComp.Type != LightType::Directional)
+						{
+							float range = lightComp.Range;
+							if (ImGui::DragFloat("Range", &range, 0.1f, 0.0f, 1000.0f, "%.2f"))
+							{
+								lightComp.SetRange(range); // uses setter
+							}
+						}
+
+						if (lightComp.Type == LightType::Spot)
+						{
+							float spotAngle = lightComp.SpotAngleDeg;
+							if (ImGui::DragFloat("Spot Angle", &spotAngle, 0.1f, 1.0f, 179.0f, "%.2f"))
+							{
+								lightComp.SetSpotAngleDeg(spotAngle); // uses setter
+							}
+						}
+						// --- Indirect Multiplier ---
+						float indirectMult = lightComp.IndirectMultiplier;
+						if (ImGui::DragFloat("Indirect Multiplier", &indirectMult, 0.01f, 0.0f, 10.0f, "%.2f"))
+						{
+							lightComp.SetIndirectMultiplier(indirectMult); // uses setter
+						}
+
+					}
+					// ---------------------------- Remove Light Comp --------------------------
+					if (removeLightComp)
+					{
+						m_SelectedEntity.RemoveComponent<LightComponent>();
+					}
+				}
 				// ======================== Add Component Section ===============================
 				ImGui::Separator();
 				ImVec2 windowSize = ImGui::GetWindowSize(); // get Properties window sizes
@@ -1736,6 +1906,25 @@ namespace Engine
 					}
 					ImGui::EndDisabled();
 					//ImGui::SetWindowFontScale(1.0f); // Reset
+					// ------------------------ Add Light Component ----------------------------
+					bool hasLightComponent = m_SelectedEntity.HasComponent<LightComponent>();
+					ImGui::BeginDisabled(hasLightComponent);
+
+					if (ImGui::MenuItem("Light Component"))
+					{
+						if (!hasLightComponent)
+						{
+							m_SelectedEntity.AddComponent<LightComponent>();
+						}
+					}
+					if (ImGui::IsItemHovered())
+					{
+						if (!hasLightComponent)
+						{
+							ImGui::SetTooltip("Add script to this object.");
+						}
+					}
+					ImGui::EndDisabled();
 
 					ImGui::EndPopup(); // end pop up for Add Component  
 				}
