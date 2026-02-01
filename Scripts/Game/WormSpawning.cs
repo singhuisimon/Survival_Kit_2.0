04965125e6b9fb7c56a0ddd1bf8bfe12d0388961
Amanda Leow <amandaleow543@gmail.com>
2026-02-02 03:07:17 +0800

    added in the game win and lose event to more script
    - loveletter spawn will not spawn when it is game win
    - worm host will also delete itself when it is game win
    - worm spawning will also not spawn when it is game win.
    - playerweapon won't be allowed to shoot anything should game win/lose

--- Scripts/Game/WormSpawning.cs
+++ Scripts/Game/WormSpawning.cs
@@ -36,6 +36,7 @@ namespace Game {
 
         // Game lose condition
         private const string GAMEOVER = "GameOver";
+        private const string GAMEWIN = "GameWin";
 
         // Game win condition
 
@@ -59,6 +60,7 @@ namespace Game {
             gameRunning = true;
 
             Event.Subscribe(GAMEOVER, OnGameOver);
+            Event.Subscribe(GAMEWIN, OnGameOver);
         }
 
         public override void OnUpdate(float deltaTime){
@@ -88,6 +90,7 @@ namespace Game {
 
         public override void OnDestroy(){
             Event.Unsubscribe(GAMEOVER, OnGameOver);
+            Event.Unsubscribe(GAMEWIN, OnGameOver);
         }
 
         private void SpawnWormOnRandomWall(){
