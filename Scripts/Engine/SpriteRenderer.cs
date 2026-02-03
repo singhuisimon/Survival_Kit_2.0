6b9766e6a992d44601202d55c6929f7312569e4d
Simon Chan Sing Hui <sh.simon.c.study@pm.me>
2026-02-03 16:45:08 +0800

    Implemented cut scenes

--- Scripts/Engine/SpriteRenderer.cs
+++ Scripts/Engine/SpriteRenderer.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 namespace Engine
 {
     /// <summary>
-    /// SpriteRenderer component bindings for controlling 2D sprite visibility
+    /// SpriteRenderer component bindings for controlling 2D sprite visibility and color
     /// </summary>
     public static class SpriteRenderer
     {
@@ -12,6 +12,12 @@ namespace Engine
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern bool SpriteRenderer_GetIsVisible(uint entityID);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void SpriteRenderer_SetColor(uint entityID, float r, float g, float b, float a);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void SpriteRenderer_GetColor(uint entityID, out float r, out float g, out float b, out float a);
 
         /// <summary>
         /// Set sprite visibility (show/hide)
@@ -27,6 +33,22 @@ namespace Engine
         public static bool GetIsVisible(uint entityID)
         {
             return SpriteRenderer_GetIsVisible(entityID);
+        }
+
+        /// <summary>
+        /// Set sprite color (RGBA)
+        /// </summary>
+        public static void SetColor(uint entityID, float r, float g, float b, float a)
+        {
+            SpriteRenderer_SetColor(entityID, r, g, b, a);
+        }
+
+        /// <summary>
+        /// Get sprite color (RGBA)
+        /// </summary>
+        public static void GetColor(uint entityID, out float r, out float g, out float b, out float a)
+        {
+            SpriteRenderer_GetColor(entityID, out r, out g, out b, out a);
         }
     }
 }
