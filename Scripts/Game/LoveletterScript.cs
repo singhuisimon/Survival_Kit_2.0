5a7c67d077a547095797338b8b6e69b10409aca4
Amanda Leow <amandaleow543@gmail.com>
2026-02-04 21:36:52 +0800

    Fix loveletter not dying bug as well as added in the 3 new gunship and the proper core.

--- Scripts/Game/LoveletterScript.cs
+++ Scripts/Game/LoveletterScript.cs
@@ -225,8 +225,10 @@ namespace Game
                 return;
             float damage = DamageSystem.ParseAmount(payload);
             currentHealth -= damage;
-            if (currentHealth < 0.0f)
+            
+            if (currentHealth < 0.0f){
                 currentHealth = 0.0f;
+            }
 
             uint attackerId = DamageSystem.ParseAttackerId(payload);
             if(attackerId != INVALID_ENTITY){
@@ -242,6 +244,12 @@ namespace Game
                         LogMessage("[LoveletterScript] Player Hit! But hitmarkerID fail to instantiate");
                     }
                 }
+            }
+
+            if (currentHealth <= 0.0f)
+            {
+                Publish("LoveLetterDeath", 1.ToString());
+                DestroyLoveLetter();
             }
         }
 
