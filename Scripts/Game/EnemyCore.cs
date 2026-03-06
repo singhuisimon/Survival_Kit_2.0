00c1a0a9058c02fad34c6d8cbf81b8a4a23ea870
ashleyThit <144297753+ashleyThit@users.noreply.github.com>
2026-03-06 16:14:04 +0800

    add Damage UI to enemycore

--- Scripts/Game/EnemyCore.cs
+++ Scripts/Game/EnemyCore.cs
@@ -10,8 +10,8 @@ namespace Game
 {
     public class EnemyCore : ScriptBehaviour
     {
-        [SerializeField] private const float MaxHealth = 150.0f;
-        [SerializeField] private float CurrentHealth = 150.0f;
+        [SerializeField] private const float MaxHealth = 100.0f;
+        [SerializeField] private float CurrentHealth = 100.0f;
         [SerializeField] private float prevHealth = 0.0f;
         [SerializeField] private bool isDead = false;
 
--- Scripts/Game/EnemyCoreHPCount.cs
+++ Scripts/Game/EnemyCoreHPCount.cs
@@ -9,7 +9,7 @@ namespace Game
     public class EnemyCoreHPCount : ScriptBehaviour
     {
         private const string EVENT_ENEMYCORE_HEALTHCHANGE = "EnemyCore Health Change";
-        private const float MaxHealth = 150.0f;
+        private const float MaxHealth = 100.0f;
 
         private const string EVENT_PLAYER_DEAD = "PlayerDead";
         private const string EVENT_CORE_DESTROYED = "CoreMotherboardDestroyed";
