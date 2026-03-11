2d1e6d5628908e72367bae9287d2ae64342c4f86
nikelora <amandaleow543@gmail.com>
2026-03-11 10:08:03 +0800

    created 2 audio prefab for spawning the win audio for both levels. Added in scripts related to 2 other audio (core health vo and level2 bgm vo)
    Made changes to the winscreen script to spawn after a slight delay for the win audio regarding both levels

--- Scripts/Game/WinScreen.cs
+++ Scripts/Game/WinScreen.cs
@@ -4,6 +4,7 @@ using static Engine.Logger;
 using static Engine.SpriteRenderer;
 using static Engine.Event;
 using static Engine.AudioManager;
+using static Engine.Prefab;
 
 namespace Game
 {
@@ -19,6 +20,17 @@ namespace Game
         private const string GAMEWIN = "GameWin";
 
         private bool initialized = false;
+        private bool countdownstart = false;
+        private bool spawnedVO = false;
+
+        [SerializeField] private float countdown = 0.0f;
+
+        private const float countdownlevel2 = 0.7f;
+        private const float countdowntrench = 0.5f;
+
+        private string winVOPrefab = "";
+        private const string winTrenchVOPrefab = "Sources/Prefabs/Audio_Win_Trench_VO.prefab";
+        private const string winLevel2VOPrefab = "Sources/Prefabs/Audio_Win_Level2_VO.prefab";
 
         public override void OnStart()
         {
@@ -26,8 +38,8 @@ namespace Game
             LogMessage("WinScreen EntityID: " + EntityID);
 
             // Subscribe to timer finished
-            Event.Subscribe(EVENT_TIMER_FINISHED, OnWin);
-            Event.Subscribe(ENEMY_CORE_DEATH, OnWin);
+            Event.Subscribe(EVENT_TIMER_FINISHED, OnLevel2Win);
+            Event.Subscribe(ENEMY_CORE_DEATH, OnTrenchWin);
 
             // Start hidden
             SetIsVisible((uint)EntityID, false);
@@ -36,11 +48,43 @@ namespace Game
             LogMessage("[WinScreen] Initialized - waiting for timer to finish");
         }
 
-        private void OnWin(string eventName, string payload)
+        private void OnUpdate(float deltaTime){
+
+            if(countdownstart){
+                countdown -= deltaTime;
+
+                if(countdown <= 0.0f && !spawnedVO){
+                    uint winVOID = 0;
+                    winVOID = PrefabInstantiate(winVOPrefab);
+                    if(winVOID == 0){
+                        LogMessage("[WinScreen] Failed to instantiate win voiceover for audio: " + winVOPrefab);
+                    }
+
+                    spawnedVO = true;
+                }
+            }
+
+        }
+
+        private void OnTrenchWin(string eventName, string payload)
+        {
+            LogMessage("[WinScreen] Win! Trench run finished!");
+            StopGroup(AudioType.MASTER);
+            winVOPrefab = winTrenchVOPrefab;
+            countdownstart = true;
+            countdown = countdowntrench;
+            Input.SetCursorVisible(true);
+            SetIsVisible((uint)EntityID, true);
+            Publish(GAMEWIN, "");
+        }
+
+        private void OnLevel2Win(string eventName, string payload)
         {
             LogMessage("[WinScreen] Win! Timer finished!");
-            StopGroup(AudioType.BGM);
-            StopGroup(AudioType.SFX);
+            StopGroup(AudioType.MASTER);
+            winVOPrefab = winLevel2VOPrefab;
+            countdownstart = true;
+            countdown = countdownlevel2;
             Input.SetCursorVisible(true);
             SetIsVisible((uint)EntityID, true);
             Publish(GAMEWIN, "");
@@ -48,9 +92,13 @@ namespace Game
 
         public override void OnDestroy()
         {
-            Event.Unsubscribe(EVENT_TIMER_FINISHED, OnWin);
-            Event.Unsubscribe(ENEMY_CORE_DEATH, OnWin);
+            Event.Unsubscribe(EVENT_TIMER_FINISHED, OnLevel2Win);
+            Event.Unsubscribe(ENEMY_CORE_DEATH, OnTrenchWin);
             LogMessage("=== WinScreen Destroyed ===");
+        }
+
+        private void AfterWin(){
+
         }
     }
 }
