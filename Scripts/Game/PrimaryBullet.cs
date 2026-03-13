217834e07c895cfb1a0107a45088125dda48e1f4
nikelora <amandaleow543@gmail.com>
2026-03-13 11:38:18 +0800

    changed the primary bullet damage from 5 to 1

--- Scripts/Game/PrimaryBullet.cs
+++ Scripts/Game/PrimaryBullet.cs
@@ -19,7 +19,7 @@ namespace Game
         public float ProjectileLifetime = 2.0f;
 
         [SerializeField]
-        public float Damage = 5.0f;
+        public float Damage = 1.0f;
 
         [SerializeField]
         public int UltRecharged = 1;
