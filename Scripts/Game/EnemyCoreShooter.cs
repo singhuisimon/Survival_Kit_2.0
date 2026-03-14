a914e55426eb6b4973ac43340df08f65899cafd5
liliahana <148671404+liliahana@users.noreply.github.com>
2026-03-14 21:57:12 +0800

    EnemyCore shooting stops creating shooting noise when game wins
    Also ensure worm/turret/core bullets stop moving entirely (angular velocity is set to 0 at pause)

--- Scripts/Game/EnemyCoreShooter.cs
+++ Scripts/Game/EnemyCoreShooter.cs
@@ -28,7 +28,7 @@ namespace Game
         // Shooting
         [SerializeField] private float shootingCooldown = 0.1f;
         private float shootingTimer = 0.0f;
-
+        private bool isDead = false;
         string EnemyTurretBulletPrefabPath = "Sources/Prefabs/EnemyTurretBullet.prefab";
 
         // Lifecycle
@@ -51,6 +51,9 @@ namespace Game
 
         public override void OnUpdate(float deltaTime)
         {
+            if (GameState.IsPaused || isDead)
+                return;
+
             if (playerID == INVALID_ENTITY)
                 return;
 
@@ -203,7 +206,9 @@ namespace Game
             return q;
         }
 
-        private void OnGameOver(string eventName, string payload){
+        private void OnGameOver(string eventName, string payload)
+        {
+            isDead = true;
             SceneDestroyEntity(EntityID);
         }
     }
