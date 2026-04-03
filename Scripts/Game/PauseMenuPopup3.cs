6b02110f469cbd6d426f4e06c51d3d7ffc0586c7
literallyjoyboy <kuekweijie@gmail.com>
2026-04-03 16:15:33 +0800

    pausemenu bug fix

--- Scripts/Game/PauseMenuPopup3.cs
+++ Scripts/Game/PauseMenuPopup3.cs
@@ -72,7 +72,7 @@ namespace Game
             "PlayerHealth", "PrimaryWeaponFrame", "PrimaryWeaponWindow",
             "SecondayWeaponUI", "AltFireIcon", "WeaponBarReload",
             "CoreHealthBarRefill", "CoreHealthBarFrame", "CoreHealthBarWindow",
-            "Crosshair", "Crosshair2","HealBar","DamageBarFill","timerUp","SentryWindow"
+            "Crosshair", "Crosshair2","HealBar","DamageBarFill","timerUp","SentryWindow","SentryCount"
         };
 
         // Scene paths
